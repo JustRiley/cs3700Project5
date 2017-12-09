3686122e3ba0c5dddffd35e2b4ec9c0c24c0b787
perrydai <perrydaiii@gmail.com>
2017-12-08 21:06:10 -0500

    occasionally pass first 13 tests

--- project5/test.py
+++ project5/test.py
@@ -73,35 +73,35 @@ def run_test(filename, description, requests, replicas, mayfail, tolerance, late
 
 trials = []
 
-#print 'Basic tests (5 replicas, 30 seconds, 500 requests):'
-#trials.append(run_test('simple-1.json', 'No drops, no failures, 80% read',
-#                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.2, LATENCY_LOW))
-#trials.append(run_test('simple-2.json', 'No drops, no failures, 20% read',
-#                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.2, LATENCY_LOW))
+print 'Basic tests (5 replicas, 30 seconds, 500 requests):'
+trials.append(run_test('simple-1.json', 'No drops, no failures, 80% read',
+                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.2, LATENCY_LOW))
+trials.append(run_test('simple-2.json', 'No drops, no failures, 20% read',
+                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.2, LATENCY_LOW))
 
 print 'Unreliable network tests (5 replicas, 30 seconds, 500 requests):'
-#trials.append(run_test('unreliable-1.json', '5% drops, no failures, 20% read',
-#                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.25, LATENCY_LOW))
-#trials.append(run_test('unreliable-2.json', '10% drops, no failures, 20% read',
-#                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.3, LATENCY_MID))
+trials.append(run_test('unreliable-1.json', '5% drops, no failures, 20% read',
+                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.25, LATENCY_LOW))
+trials.append(run_test('unreliable-2.json', '10% drops, no failures, 20% read',
+                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.3, LATENCY_MID))
 trials.append(run_test('unreliable-3.json', '15% drops, no failures, 20% read',
                        PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.35, LATENCY_MID))
 
 print 'Crash failure tests (5 replicas, 30 seconds, 500 requests):'
-#trials.append(run_test('crash-1.json', 'No drops, 1 replica failure, 20% read',
-#                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.2, LATENCY_LOW))
-#trials.append(run_test('crash-2.json', 'No drops, 2 replica failures, 20% read',
-#                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.2, LATENCY_LOW))
-#trials.append(run_test('crash-3.json', 'No drops, 1 leader failure, 20% read',
-#                       PACKETS_LOW, REPLICAS, MAYFAIL_HIGH, 1.3, LATENCY_LOW))
+trials.append(run_test('crash-1.json', 'No drops, 1 replica failure, 20% read',
+                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.2, LATENCY_LOW))
+trials.append(run_test('crash-2.json', 'No drops, 2 replica failures, 20% read',
+                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.2, LATENCY_LOW))
+trials.append(run_test('crash-3.json', 'No drops, 1 leader failure, 20% read',
+                       PACKETS_LOW, REPLICAS, MAYFAIL_HIGH, 1.3, LATENCY_LOW))
 trials.append(run_test('crash-4.json', 'No drops, 2 leader failures, 20% read',
                        PACKETS_LOW, REPLICAS, MAYFAIL_HIGH, 1.4, LATENCY_LOW))
 
 print 'Partition tests (5 replicas, 30 seconds, 500 requests):'
-#trials.append(run_test('partition-1.json', 'No drops, 1 easy partition, 20% read',
-#                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.3, LATENCY_LOW))
-#trials.append(run_test('partition-2.json', 'No drops, 2 easy partitions, 20% read',
-#                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.4, LATENCY_LOW))
+trials.append(run_test('partition-1.json', 'No drops, 1 easy partition, 20% read',
+                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.3, LATENCY_LOW))
+trials.append(run_test('partition-2.json', 'No drops, 2 easy partitions, 20% read',
+                       PACKETS_LOW, REPLICAS, MAYFAIL_LOW, 1.4, LATENCY_LOW))
 trials.append(run_test('partition-3.json', 'No drops, 1 hard partition, 20% read',
                        PACKETS_LOW, REPLICAS, MAYFAIL_HIGH, 1.5, LATENCY_HIGH))
 trials.append(run_test('partition-4.json', 'No drops, 2 hard partitions, 20% read',
